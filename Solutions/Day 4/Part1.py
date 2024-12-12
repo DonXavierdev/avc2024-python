46650114d40abaf74829e9a88a778c0c726fc8bc
Don Xavier <mail.donxavier@gmail.com>
2024-12-12 23:01:54 +0530

    Day 4 part 4 directions established

--- Solutions/Day 4/Part1.py
+++ Solutions/Day 4/Part1.py
@@ -3,9 +3,21 @@
 # if the next letter found in one of the directions repeat process in same direction
 # If all letters found increment count
 
+directions = [[-1,0],[1,0],[0,1],[0,-1]]
 
+def dfs(r,c,):
+    for x,y in directions:
+        new_r = r + x
+        new_c = c + y
+        if 0 <= new_c < len(matrix_puzz[0]) and 0 <= new_r < len(matrix_puzz):
+            print(matrix_puzz[new_r][new_c])
 with open('input.txt','r') as f:
     puzz = f.readlines()
 matrix_puzz = []
 for i in puzz:
     matrix_puzz.append(list(i[:-1]))
+for r in range(len(matrix_puzz)):
+    for c in range(len(matrix_puzz[0])):
+        if matrix_puzz[r][c] == 'X':
+            print(r,c)
+            dfs(r,c)
